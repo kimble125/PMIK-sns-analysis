8cc1fcf3fec9982562e49e85f99f53cfe6df9114
kimble125 <hoykim125@naver.com>
2025-11-02 03:06:54 +0900

    Update Naver API client credentials
    Updated Naver API client credentials in config.py.

--- naver_blog/config.py
+++ naver_blog/config.py
@@ -2,5 +2,5 @@
 # 실제 사용시 이 파일을 config.py로 복사하고 실제 키를 입력하세요
 # https://developers.naver.com/apps/#/register 에서 발급받으세요
 
-NAVER_CLIENT_ID = 'your_client_id_here'
-NAVER_CLIENT_SECRET = 'your_client_secret_here'
+NAVER_CLIENT_ID = '9v7cOolOk2ctSQXc73sd'
+NAVER_CLIENT_SECRET = '9jHcXVNQwZ'
